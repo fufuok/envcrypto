9bceaf056d2fd3dcd52920e7bab9396b2c2d15f8
Fufu <fufuok@qq.com>
2021-05-06 13:52:56 +0800

    Update: add example and unittest

---
+++ example.py
@@ -0,0 +1,132 @@
+# -*- coding:utf-8 -*-
+"""
+    example.py
+    ~~~~~~~~
+
+    :author: Fufu, 2021/5/6
+"""
+from envcrypto.crypto import *
+
+if __name__ == '__main__':
+    # Recommended usage
+    api_secret = 'AABB.Fufu@TEST001'
+    env_name = 'APP_API_SECRET'
+    my_key = b'\02S\n\t@k\x02'  # Any type and any length
+    set_res = set_environ(env_name, api_secret, my_key)
+    get_res = get_environ(env_name, key=my_key)
+    print('''Raw data[{0}]: {1!r}
+Set environment variable in your OS:
+    export {2}={3}
+In your code (result is string):
+    result = get_environ(env_name, key=my_key)
+    result[{4}] == api_secret == {5!r}
+'''.format(type(api_secret), api_secret, env_name, set_res, type(get_res), get_res))
+
+    # if key is None
+    set_res = set_environ(env_name, api_secret)
+    get_res = get_environ(env_name)
+    print('''Raw data[{0}]: {1!r}
+Set environment variable in your OS (if key is None):
+    export {2}={3}
+In your code (result is string):
+    result = get_environ(env_name)
+    result[{4}] == api_secret == {5!r}
+'''.format(type(api_secret), api_secret, env_name, set_res, type(get_res), get_res))
+
+    # if raw data is bytes
+    secret_bytes = b'a random bytes \xff&\xd2...'
+    set_res = set_environ(env_name, secret_bytes, my_key)
+    get_res = get_environ(env_name, key=my_key)
+    print('''Raw data[{0}]: {1!r}
+Set environment variable in your OS:
+    export {2}={3}
+In your code (result is bytes):
+    result = get_environ(env_name, key=my_key)
+    result[{4}] == secret_bytes == {5!r}
+'''.format(type(secret_bytes), secret_bytes, env_name, set_res, type(get_res), get_res))
+
+    print('No default value:', get_environ('NOT_EXIST'))
+    print('Has a default value:', get_environ('NOT_EXIST', default='default.value'))
+    print()
+
+    # Some additional extension methods
+    bytes_data = b'Hello bytes'
+    any_key = False
+    en_res = encrypt(bytes_data, any_key)
+    de_res = decrypt(en_res, any_key)
+    # en_res[<class 'str'>] = 'YjVMkROX3hNyt9mEJfwBvfI='
+    # de_res[<class 'bytes'>] == bytes_data[<class 'bytes'>] == b'Hello bytes'
+    print('en_res[{0}] = {1!r}\n'
+          'de_res[{2}] == bytes_data[{3}] == {4!r}\n'.format(type(en_res), en_res,
+                                                             type(de_res), type(bytes_data), de_res))
+
+    # Not bytes, the result will be string
+    any_data = 123.45
+
+    # use key: b'False\x1b\x1b\x1b\x1b\x1b\x1b\x1b\x1b\x1b\x1b\x1b\x1b\x1b\x1b\x1b\x1b\x1b
+    # \x1b\x1b\x1b\x1b\x1b\x1b\x1b\x1b\x1b\x1b'
+    # use iv: b'False\x1b\x1b\x1b\x1b\x1b\x1b\x1b\x1b\x1b\x1b\x1b'
+    # encrypted: b's\xd5\xd4\x91\xa6olv\x8b\x0e0,\xa3/\x83\xa2_'
+    # encrypted: b's\xd5\xd4\x91\xa6olv\x8b\x0e0,\xa3/\x83\xa2_'
+    # decrypted: 123.45
+    # decrypted: 123.45
+    print('use key:', AESCipher(any_key).key)
+    print('use iv:', AESCipher(any_key).iv)
+    print('encrypted:', AESCipher(any_key).encrypt_aes_cbc(any_data))
+    print('encrypted:', encrypt_aes_cbc(any_data, any_key))
+    print('decrypted:', AESCipher(any_key).decrypt_aes_cbc(b's\xd5\xd4\x91\xa6olv\x8b\x0e0,\xa3/\x83\xa2_'))
+    print('decrypted:', decrypt_aes_cbc(b's\xd5\xd4\x91\xa6olv\x8b\x0e0,\xa3/\x83\xa2_', any_key))
+
+    # res[<class 'str'>] = '123.45'
+    res = decrypt_aes_cbc(b's\xd5\xd4\x91\xa6olv\x8b\x0e0,\xa3/\x83\xa2_', any_key)
+    print('res[{0}] = {1!r}'.format(type(res), res))
+    print()
+
+    # Standard AES-CBC-128 pkcs7
+    my_data = b'Fufu'
+    my_key = b'0123456789012345'
+    my_iv = b'1234567890123456'
+    # GUV0s3zVssASrOsESlepWA==
+    print(b64encode(encrypt_aes_cbc(my_data, my_key, my_iv, 128)[1:]).decode('utf-8'))
+    print(AESCipher(my_key, my_iv, 128, 'pkcs7', with_type=False).encrypt_aes_cbc_b64(my_data))
+
+    # In: string, Out: string
+    my_data = 'Fufu .$\x0f\r\ni^★地99'
+    my_key = b'ff\xf4YF7777777\x024\x66~\xa7\xb6\x5c12356'
+
+    a = encrypt(my_data, my_key)
+    b = decrypt(a, my_key)
+    # a:'c-e7NIstGdi6okPbavSbNs6M1oYC8aZhHBokVcpRvQJ5'
+    # b:'Fufu .$\x0f\r\ni^★地99'
+    # b==my_data:True
+    print('\na:{0!r}\nb:{1!r}\nb==my_data:{2!r}'.format(a, b, b == my_data))
+
+    # encrypt aes-cbc
+    ciphertext_bytes = encrypt_aes_cbc(my_data, my_key)
+    ciphertext_hex = encrypt_aes_cbc_hex(my_data, my_key)
+    ciphertext_b64 = encrypt_aes_cbc_b64(my_data, my_key)
+    ciphertext_urlsafeb64 = encrypt_aes_cbc_urlsafe_b64(my_data, my_key)
+    # bytes:b's\xbb\xff#z\x95\xcb7*\x8d\xca\x00\x1e\xb7\xcaN\xf8S\x05\xe5\x80\xcb\x9d#dr\xb6vp\xf6\xe0\x9c\x96'
+    # hex:73bbff237a95cb372a8dca001eb7ca4ef85305e580cb9d236472b67670f6e09c96
+    # b64:c7v/I3qVyzcqjcoAHrfKTvhTBeWAy50jZHK2dnD24JyW
+    # urlsafeb64:c7v_I3qVyzcqjcoAHrfKTvhTBeWAy50jZHK2dnD24JyW
+    print('\nbytes:{}\nhex:{}\nb64:{}\nurlsafeb64:{}'.format(
+        ciphertext_bytes, ciphertext_hex, ciphertext_b64, ciphertext_urlsafeb64))
+
+    # decrypt aes-cbc
+    plaintext_1 = decrypt_aes_cbc(ciphertext_bytes, my_key)
+    plaintext_2 = decrypt_aes_cbc_hex(ciphertext_hex, my_key)
+    plaintext_3 = decrypt_aes_cbc_b64(ciphertext_b64, my_key)
+    plaintext_4 = decrypt_aes_cbc_urlsafe_b64(ciphertext_urlsafeb64, my_key)
+    # 'Fufu .$\x0f\r\ni^★地99'
+    # 'Fufu .$\x0f\r\ni^★地99'
+    # 'Fufu .$\x0f\r\ni^★地99'
+    # 'Fufu .$\x0f\r\ni^★地99'
+    # True
+    print('\n{0!r}\n{1!r}\n{2!r}\n{3!r}\n{4}'.format(
+        plaintext_1, plaintext_2, plaintext_3, plaintext_4, plaintext_4 == my_data))
+
+    # Generate a random key
+    # random_key = os.urandom(16)
+    random_key = AESCipher(bits=128).key
+    print(random_key)
---
+++ test_envcrypto.py
@@ -0,0 +1,133 @@
+# -*- coding:utf-8 -*-
+"""
+    test_envcrypto.py
+    ~~~~~~~~
+
+    :author: Fufu, 2021/5/6
+"""
+import unittest
+from envcrypto.crypto import *
+
+
+class TestEnvcrypto(unittest.TestCase):
+    def setUp(self) -> None:
+        self.raw_str = 'Fufu .$\x0f\r\ni^★地99'
+        self.raw_bytes = b'ff\xf4YF7777777\x024\x66~\xa7\xb6\x5c12356'
+        self.raw_list = ['A', '中', '', 1.23, True, {'x': (2, None)}]
+        self.env_name = 'TEST_ENVCRYPTO'
+        self.key = b'\02S\n\t@k\x02' * 100
+        self.iv = 123
+
+    def test_environ_str(self):
+        env_value = set_environ(self.env_name, self.raw_str, self.key)
+        self.assertIsInstance(env_value, str)
+        self.assertIsNotNone(urlsafe_b64decode(env_value))
+
+        get_env_value = get_environ(self.env_name)
+        self.assertEqual(get_env_value, env_value)
+        self.assertEqual(get_env_value, encrypt(self.raw_str, self.key))
+
+        get_value = get_environ(self.env_name, self.key)
+        self.assertIsInstance(get_value, str)
+        self.assertEqual(get_value, self.raw_str)
+        self.assertEqual(get_value, decrypt(get_env_value, self.key))
+
+    def test_environ_bytes(self):
+        env_value = set_environ(self.env_name, self.raw_bytes, self.key)
+        self.assertIsInstance(env_value, str)
+        self.assertIsNotNone(urlsafe_b64decode(env_value))
+
+        get_env_value = get_environ(self.env_name)
+        self.assertEqual(get_env_value, env_value)
+        self.assertEqual(get_env_value, encrypt(self.raw_bytes, self.key))
+
+        get_value = get_environ(self.env_name, self.key)
+        self.assertIsInstance(get_value, bytes)
+        self.assertEqual(get_value, self.raw_bytes)
+        self.assertEqual(get_value, decrypt(get_env_value, self.key))
+
+    def test_environ_other(self):
+        env_value = set_environ(self.env_name, self.raw_list, self.key)
+        self.assertIsInstance(env_value, str)
+        self.assertIsNotNone(urlsafe_b64decode(env_value))
+
+        get_env_value = get_environ(self.env_name)
+        self.assertEqual(get_env_value, env_value)
+        self.assertEqual(get_env_value, encrypt(self.raw_list, self.key))
+
+        get_value = get_environ(self.env_name, self.key)
+        self.assertIsInstance(get_value, str)
+        self.assertEqual(get_value, str(self.raw_list))
+        self.assertEqual(get_value, decrypt(get_env_value, self.key))
+
+        env_value = set_environ(self.env_name, self.raw_list, key=None)
+        self.assertEqual(env_value, str(self.raw_list))
+        self.assertEqual(get_environ(self.env_name), str(self.raw_list))
+
+    def test_helper(self):
+        en_1 = encrypt_aes_cbc(self.raw_str, self.key)
+        en_2 = encrypt_aes_cbc_hex(self.raw_str, self.key)
+        en_3 = encrypt_aes_cbc_b64(self.raw_str, self.key)
+        en_4 = encrypt_aes_cbc_urlsafe_b64(self.raw_str, self.key)
+        self.assertIsInstance(en_1, bytes)
+        self.assertIsInstance(en_2, str)
+        self.assertIsInstance(en_3, str)
+        self.assertIsInstance(en_4, str)
+        self.assertIsNotNone(en_1)
+        self.assertIsNotNone(bytes.fromhex(en_2))
+        self.assertIsNotNone(b64decode(en_3))
+        self.assertIsNotNone(urlsafe_b64decode(en_4))
+        de_1 = decrypt_aes_cbc(en_1, self.key)
+        de_2 = decrypt_aes_cbc_hex(en_2, self.key)
+        de_3 = decrypt_aes_cbc_b64(en_3, self.key)
+        de_4 = decrypt_aes_cbc_urlsafe_b64(en_4, self.key)
+        self.assertEqual(de_1, self.raw_str)
+        self.assertEqual(de_2, self.raw_str)
+        self.assertEqual(de_3, self.raw_str)
+        self.assertEqual(de_4, self.raw_str)
+
+    def test_std_aes_cbc(self):
+        for bits in [128, 192, 256]:
+            aes = AESCipher(self.key, self.iv, bits=bits, style='pkcs7', with_type=False)
+            en = aes.encrypt_aes_cbc(self.raw_bytes)
+            en_helper = encrypt_aes_cbc(self.raw_bytes, self.key, self.iv, bits)
+            self.assertEqual(en, en_helper[1:])
+            de = aes.decrypt_aes_cbc(en)
+            self.assertEqual(de, self.raw_bytes)
+            self.assertEqual(de, decrypt_aes_cbc(en_helper, self.key, self.iv, bits))
+
+    def test_other(self):
+        key = False
+        use_key = AESCipher(key).key
+        use_iv = AESCipher(key).iv
+        self.assertEqual(use_key, b'False\x1b\x1b\x1b\x1b\x1b\x1b\x1b\x1b\x1b\x1b\x1b\x1b\x1b\x1b\x1b'
+                                  b'\x1b\x1b\x1b\x1b\x1b\x1b\x1b\x1b\x1b\x1b\x1b\x1b')
+        self.assertEqual(use_key[:AES.block_size], use_iv)
+
+        random_key = AESCipher().key
+        self.assertEqual(len(random_key), 256 / 8)
+        self.assertIsInstance(random_key, bytes)
+        random_key = AESCipher(bits=128).key
+        self.assertEqual(len(random_key), 128 / 8)
+        self.assertIsInstance(random_key, bytes)
+        random_key_100 = AESCipher().mk_key(length=100)
+        self.assertEqual(len(random_key_100), 100)
+
+        raw = True
+        aes = AESCipher()
+        res = aes.decrypt_aes_cbc(aes.encrypt_aes_cbc(raw))
+        self.assertEqual(res, 'True')
+        self.assertTrue(bool(res))
+
+        tmp = TmpClass()
+        res = decrypt_aes_cbc_b64(encrypt_aes_cbc_b64(tmp, self.key), self.key)
+        self.assertEqual(res, 'I am TmpClass.')
+
+
+class TmpClass:
+    def __str__(self):
+        return 'I am TmpClass.'
+
+
+if __name__ == '__main__':
+    unittest.main()
